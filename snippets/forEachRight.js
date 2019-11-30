e4bda206d5564f67ad831660d06da9ff6f63f85d
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-11-30 23:54:37 +0300

    Create forEachRight.js

---
+++ snippets/forEachRight.js
@@ -0,0 +1,13 @@
+// Executes a provided function once for each array element, starting from the array's last element.
+
+// slice(0) to clone the given array, reverse() to reverse it and forEach() to iterate over the reversed array.
+
+const forEachRight = (arr, callback) =>
+  arr
+    .slice(0)
+    .reverse()
+    .forEach(callback);
+    
+// Exp.
+  forEachRight([1, 2, 3, 4], val => console.log(val));
+  // '4', '3', '2', '1'
