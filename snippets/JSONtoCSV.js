030ec0044c205c0d16bba15c2ac3c6f9e57ce7fb
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-12-05 20:22:18 +0300

    Create JSONtoCSV.js

---
+++ snippets/JSONtoCSV.js
@@ -0,0 +1,17 @@
+// Converts an array of objects to a ',' values string that contains only the columns specified.
+
+const JSONtoCSV = (arr, columns, delimiter = ',') =>
+  [
+    columns.join(delimiter),
+    ...arr.map(obj => 
+      columns.reduce(
+        (acc, key) => `${acc}${!acc.length ? '' :
+      delimiter}"${!obj[key] ? '' : obj[key]}"`,
+      ''
+      )
+    )
+  ].join('\n');
+  
+// Exp.
+  JSONtoCSV([{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }], ['a', 'b']);
+  // 'a,b\n"1", "2"\n"3", "4"\n"6", ""\n"", "7"'
