972b88d4613c3ffe34b1aee1bdd8d0201c798aeb
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-12-07 12:12:09 +0300

    Create reducedFilter.js

---
+++ snippets/reducedFilter.js
@@ -0,0 +1,28 @@
+// Filter an array of objects based on a condition while also filtering out unspecified keys.
+
+// filter() to filter the array based on the predicate fn so that it returns the objects for which the condition returned a truthy value. On the filtered array, map() to return the new object using reduce() to filter out the keys which were not supplied as the keys argument.
+
+const reducedFilter = (data, keys, fn) =>
+  data.filter(fn).map(el =>
+    keys.reduce((acc, key) => {
+      acc[key] = el[key];
+      return acc;
+    }, 
+    {}
+  ));
+
+// Exp.
+  const data = [
+    {
+      id: 1,
+      name: 'john',
+      age: 24
+    },
+    {
+      id: 2,
+      name: 'mike',
+      age: 50
+    }
+  ];
+  reducedFilter(data, ['id', 'name'], item => item.age > 24);
+  // [{ id: 2, name: 'mike'}]
