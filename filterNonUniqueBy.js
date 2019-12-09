49a3b4ef88423639a18b5e39b2df257e0567a8d4
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-12-09 23:59:23 +0300

    Create filterNonUniqueBy.js

---
+++ filterNonUniqueBy.js
@@ -0,0 +1,19 @@
+// Filters out the non-unique values in an array, based on a provided comparator function.
+
+// filter() and every() for an array containing only the unique values, based on the comparator function, fn.
+
+const filterNonUniqueBy = (arr, fn) =>
+  arr.filter((v, i) => arr.every((x, j) => (i === j) === fn(v, x, i, j)));
+  
+// Exp.
+  filterNonUniqueBy(
+    [
+      { id: 0, value: 'a' },
+      { id: 1, value: 'b' },
+      { id: 2, value: 'c' },
+      { id: 1, value: 'd' },
+      { id: 0, value: 'e' }
+    ],
+    (a, b) => a.id == b.id
+  ); 
+  // [ { id: 2, value: 'c' } ]
