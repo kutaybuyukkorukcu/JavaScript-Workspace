37ce74600aeefc2b3d9a770ec329d214f579bafd
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2019-12-18 00:14:05 +0300

    Create uniqueElementsBy.js

---
+++ snippets/uniqueElementsBy.js
@@ -0,0 +1,23 @@
+// Returns all unique values of an array, based on a provided comparator function.
+
+// reduce() and some() for an array containing only the first unique occurrence of each value, based on the comparator function, fn.
+// The comparator function takes two arguments : the values of the two elements being compared.
+
+const uniqueElementsBy = (arr, fn) =>
+  arr.reduce((acc, v) => {
+    if (!acc.some(x => fn(v, x))) acc.push(v);
+    return acc;
+  }, []);
+  
+// Exp.
+  uniqueElementsBy(
+    [
+      { id: 0, value: 'a' },
+      { id: 1, value: 'b' },
+      { id: 2, value: 'c' },
+      { id: 1, value: 'd' },
+      { id: 0, value: 'e' }
+    ],
+    (a, b) => a.id == b.id
+  ); 
+  // [ { id: 0, value: 'a' }, { id: 1, value: 'b' }, { id: 2, value: 'c' } ]
