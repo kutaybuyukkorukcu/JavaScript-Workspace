af169e1ea83a36fbb5ef37f0d5217d7e105c1b0c
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-02-21 23:57:59 +0300

    Create UUIDGeneratorNode.js

---
+++ snippets/UUIDGeneratorNode.js
@@ -0,0 +1,13 @@
+// Generates a UUID in NodeJS.
+
+// using "crypto" API to generate a UUID, compliant with RFC4122 v4.
+
+const crypto = require('crypto');
+const UUIDGeneratorNode = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ (crypto.randomBytes(1)[0] & (15 >> (c / 4)))).toString(16)
+  );
+  
+// Exp.
+  UUIDGeneratorNode();
+  // '79c7c136-60ee-40a2-beb2-856f1feabefc'
