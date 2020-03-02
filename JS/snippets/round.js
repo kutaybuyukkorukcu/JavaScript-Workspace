85e0881efbb04df463d5b3780ab35c3baf96e4fb
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-03-02 17:40:39 +0300

    Create round.js

---
+++ JS/snippets/round.js
@@ -0,0 +1,10 @@
+// Rounds a number to a specified amount of digits.
+
+// Using round() and template literals to round the number to the specified number of digits. Omit the second argument, decimals to round to an integer.
+
+const round = (n, decimals = 0) => 
+          Number(`${Math.round(`${n}e${decimals}`)}e-${decimals}`);
+
+// Exp.
+  round(1.005, 2);
+  // 1.01
