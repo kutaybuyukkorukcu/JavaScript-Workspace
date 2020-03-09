aa238e90ef90592ee4f5dc63520e938c256f70b8
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-03-10 00:16:54 +0300

    Create composeRight.js

---
+++ JS/snippets/composeRight.js
@@ -0,0 +1,14 @@
+// Performs left-to-right function composition.
+
+// Using reduce() to perform left-to-right func comp. First func can accept 1 or more args; the remaining funcs must be unary.
+
+const composeRight = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)));
+
+// Exp.
+  const add = (x, y) => x + y;
+  const square = x => x * x;
+  
+  const addAndSquare = composeRight(add, square);
+  
+  addAndSquare(1, 2); 
+  // 9
