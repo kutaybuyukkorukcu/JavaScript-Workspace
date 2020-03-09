44a5f2db5cab01e6127eaa76cb05f3398cd42ee8
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-03-10 01:04:30 +0300

    Create defer.js

---
+++ JS/snippets/defer.js
@@ -0,0 +1,10 @@
+// Defers invoking a func until the current call stack has cleared.
+
+// Using setTimeout() with a timeout of 1ms to add a new event to the browser event queue and allow the rendering engine to complete its work.
+// Also using (...) operator to supply the func with an arbitrary number of args.
+
+const defer = (fn, ...args) => setTimeout(fn, 1, ...args);
+
+// Exp.
+  defer(console.log, 'a'), console.log('b');
+  // logs 'b' then 'a'
