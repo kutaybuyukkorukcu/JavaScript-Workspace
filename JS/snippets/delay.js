b3cf6a1e463ca6a719ad87f931d66f20505912c6
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-03-10 01:11:54 +0300

    Create delay.js

---
+++ JS/snippets/delay.js
@@ -0,0 +1,14 @@
+// Invokes the provided func after wait ms.
+
+// Using setTimeout() to delay exec of 'fn'. Also using (...) operator to supply the func with an arbitrary number of args.
+
+const delay = (fn, wait, ...args) => setTimeout(fn, wait, ...args);
+
+// Exp.
+  delay(
+    (text) => {
+      console.log(text);
+    },
+    1000,
+    'Delayed text!'
+  );
