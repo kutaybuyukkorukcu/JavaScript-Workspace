6aed549d5c9cf3fc5960cab64eba76841da4d2ca
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-03-19 04:08:21 +0300

    Create isWritableStream.js

---
+++ JS/snippets/isWritableStream.js
@@ -0,0 +1,16 @@
+// Checks if the given argument is a writable stream.
+
+// Check if the value is different from null, use typeof to check if the value is of type object and the pipe property is of type function.
+// Additionally check if the typeof the _write and _writableState properties are function and object respectively.
+
+const isWritableStream = val =>
+  val !== null &&
+  typeof val === 'object' &&
+  typeof val.pipe === 'function' &&
+  typeof val._write === 'function' &&
+  typeof val._writableState === 'object';
+  
+// Exp.
+  const fs = require("fs");
+  isWritableStream(fs.createWriteStream("test.txt"));
+  // true
