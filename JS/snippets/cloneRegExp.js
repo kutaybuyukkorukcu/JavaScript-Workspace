dc3b30e73d093a8e45887a9d39486a49db682882
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-05-12 23:58:59 +0300

    Create cloneRegExp.js

---
+++ JS/snippets/cloneRegExp.js
@@ -0,0 +1,10 @@
+// Clones a regular expression.
+
+// Using new RegExp() and previous reg's values to clone.
+
+const cloneRegExp = regExp => new RegExp(regExp.source, regExp.flags);
+
+// Exp.
+    const regExp = /lorem ipsum/gi;
+    const regExp2 = cloneRegExp(regExp);
+    // -> /lorem ipsum/gi
