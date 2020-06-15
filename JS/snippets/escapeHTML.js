a5aa4863b857a019c9df96929a081689130d2f22
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-06-15 17:47:21 +0300

    Create escapeHTML.js

---
+++ JS/snippets/escapeHTML.js
@@ -0,0 +1,22 @@
+Escapes a string for use in HTML.
+
+Using String.prototype.replace() with  a regexp that matches the characters that need to be escaped, 
+using a callback function to replace each character instance with its associated escaped character using a dictionary.
+
+const escapeHTML = string => 
+    string.replace(
+      /[&<>'"]/g,
+      tag =>
+        ({
+          '&' : '&amp;',
+          '<' : '&lt;',
+          '>' : '&gt;',
+          "'" : '&#39;',
+          '"' : '&quot;'
+        }[tag] || tag)
+     );
+     
+// Exp.
+  
+  escapeHTML('<a href="#"> Me & you </a>');
+  // '&lt;a href=&quot;#&quot;&gt;Me &amp; you&lt;/a&gt;'
