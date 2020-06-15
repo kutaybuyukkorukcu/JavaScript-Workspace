df4859376f5533d56e1952d922811b9fa0c7e0e2
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-06-15 18:25:25 +0300

    Create extendHex.js

---
+++ JS/snippets/extendHex.js
@@ -0,0 +1,17 @@
+Extends a 3-digit color code to a 6-digit color code.
+
+Using map(), String.prototype.split() and join() to join the mapped array for converting 3-digit RGB notated hexadecimal color-code to the 6-digit form.
+slice() is used to remove # from string start since it's added once.
+
+const extendHex = shortHex =>
+  '#' + 
+  shortHex
+    .splice(shortHex.startsWith('#') ? 1 : 0)
+    .split('')
+    .map(x => x + x)
+    .join('');
+    
+// Exp.
+
+  extendHex('#03f');
+  // '#0033ff'
