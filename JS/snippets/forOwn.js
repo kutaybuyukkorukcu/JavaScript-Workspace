c27d3519d8c471c47f1629f7fafedfd09d4c311f
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-06-19 17:32:07 +0300

    Create forOwn.js

---
+++ JS/snippets/forOwn.js
@@ -0,0 +1,10 @@
+// Iterates over all own properties of an object, running a callback for each one.
+
+// Using Object.keys(obj) to get all the properties of the object, forEach() to run the provided func for each key-value pair.
+// The callback receives 3 args - the value, the key and the object.
+
+const forOwn = (obj, fn) => Object.keys(obj).forEach(key => fn(obj[key], key, obj));
+
+// Exp.
+  forOwn({ foo : 'bar', a : 1 }, v => console.log(v));
+  // 'bar', 1
