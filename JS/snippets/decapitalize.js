f33103234cb17dc7d14944b6d76a4454ed8ec99a
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-06-19 18:20:24 +0300

    Create decapitalize.js

---
+++ JS/snippets/decapitalize.js
@@ -0,0 +1,11 @@
+// Decapitalizes the first letter of a string.
+
+// Using array destructuring and toLowerCase() to decapitalize first letter, ...rest to get array of characters after first letter and then join(') to make it a string again.
+// Omit the upperRest parameter to keep the rest of the string intact, or set it to true to convert to uppercase.
+
+const decapitalize = ([first, ...rest], upperRest = false) =>
+  first.toLowerCase() + (upperRest ? rest.join('').toUpperCase() : rest.join(''));
+  
+// Exp.
+  decapitalize('FooBar');
+  // 'fooBar'
