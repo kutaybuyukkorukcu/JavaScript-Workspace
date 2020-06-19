8c745c9337391198fa9df6fed4bdf370252900ee
Kutay Buyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-06-19 23:26:10 +0300

    Create bindAll.js

---
+++ JS/snippets/bindAll.js
@@ -0,0 +1,25 @@
+// Binds methods of an object to the object itself, overwriting the existing method.
+
+// Using forEach() to return a func that uses Function.prototype.apply() to apply the given context obj to fn for each func specified.
+
+const bindAll = (obj, ...fns) =>
+  fns.forEach(
+    fn => (
+      (f = obj[fn]),
+      (obj[fn] = function() {
+        return f.apply(obj);
+      })
+    )
+  );
+  
+  // Exp.
+    var view = {
+      label : 'docs',
+      click : function() {
+        console.log('clicked' + this.label);
+      }
+    };
+    
+    bindAll(view, 'click');
+    jQuery(el).on('click', view.click);
+    
